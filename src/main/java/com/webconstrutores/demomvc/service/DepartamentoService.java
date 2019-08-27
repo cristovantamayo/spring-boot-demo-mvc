9d75f41521675e6d9fa9d8ed08eddcdde3e00ef3
Cristovan Tamayo <cristovan.tamayo@gmail.com>
2019-08-27 18:01:23 -0300

    Concluindo os Services

--- src/main/java/com/webconstrutores/demomvc/service/DepartamentoService.java
+++ src/main/java/com/webconstrutores/demomvc/service/DepartamentoService.java
@@ -1,5 +1,19 @@
 package com.webconstrutores.demomvc.service;
 
+import java.util.List;
+
+import com.webconstrutores.demomvc.domain.Departamento;
+
 public interface DepartamentoService {
+	
+	void salvar(Departamento departamento);
+	
+	void editar(Departamento departamento);
+	
+	void excluir(Long id);
+	
+	Departamento buscarPorId(Long id);
+	
+	List<Departamento> buscarTodos();
 
 }
