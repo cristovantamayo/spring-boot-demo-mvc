26496e5e076574c47c9fdbd733904b72a421c286
Cristovan Tamayo <cristovan.tamayo@gmail.com>
2019-08-28 10:49:12 -0300

    Inserindo Departamentos

--- src/main/java/com/webconstrutores/demomvc/web/controller/DepartamentoController.java
+++ src/main/java/com/webconstrutores/demomvc/web/controller/DepartamentoController.java
@@ -1,21 +1,35 @@
 package com.webconstrutores.demomvc.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.webconstrutores.demomvc.domain.Departamento;
+import com.webconstrutores.demomvc.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
+	
+	@Autowired
+	private DepartamentoService service;
 
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Departamento departamento) {
 		return "/departamento/cadastro";
 	}
 	
 	@GetMapping("/listar")
 	public String listar() {
 		return "/departamento/lista";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Departamento departamento) {
+		service.salvar(departamento);
+		return "redirect:/departamentos/cadastrar";
 	}
 	
 }
