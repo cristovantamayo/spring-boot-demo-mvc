22efbfee0588c44903d752c40c778d5a6048f387
Cristovan Tamayo <cristovan.tamayo@gmail.com>
2019-08-29 17:17:51 -0300

    'Java Bean Validation' no formulario de Funcionarios via ValidationMessages.properties

--- src/main/java/com/webconstrutores/demomvc/web/controller/FuncionarioController.java
+++ src/main/java/com/webconstrutores/demomvc/web/controller/FuncionarioController.java
@@ -3,10 +3,13 @@ package com.webconstrutores.demomvc.web.controller;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -44,7 +47,12 @@ public class FuncionarioController {
 	}
 	
 	@PostMapping("/salvar")
-	public String salvar(Funcionario funcionario, RedirectAttributes attr) {
+	public String salvar(@Valid Funcionario funcionario, BindingResult result, RedirectAttributes attr) {
+		
+		if(result.hasErrors()) {
+			return "/funcionario/cadastro";
+		}
+		
 		service.salvar(funcionario);
 		attr.addFlashAttribute("success", "Funcionário inserido com sucesso.");
 		return "redirect:/funcionarios/listar";
@@ -57,7 +65,12 @@ public class FuncionarioController {
 	}
 	
 	@PostMapping("/editar")
-	public String editar(Funcionario funcionario, RedirectAttributes attr) {
+	public String editar(@Valid Funcionario funcionario, BindingResult result, RedirectAttributes attr) {
+		
+		if(result.hasErrors()) {
+			return "/funcionario/cadastro";
+		}
+		
 		service.editar(funcionario);
 		attr.addFlashAttribute("success", "Funcionário editado com sucesso.");
 		return "redirect:/funcionarios/listar";
