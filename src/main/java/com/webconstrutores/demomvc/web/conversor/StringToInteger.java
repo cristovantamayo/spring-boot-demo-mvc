f8efce492aacb1d1fed9fc8e9f76013d82862bf7
Cristovan Tamayo <cristovan.tamayo@gmail.com>
2019-08-30 08:31:04 -0300

    Tratando caracteres improprios com 'Converter'

---
+++ src/main/java/com/webconstrutores/demomvc/web/conversor/StringToInteger.java
@@ -0,0 +1,21 @@
+package com.webconstrutores.demomvc.web.conversor;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StringToInteger implements Converter<String, Integer> {
+
+	@Override
+	public Integer convert(String source) {
+		
+		source = source.trim();
+		
+		if(source.matches("[0-9]+")) {
+			return Integer.valueOf(source);
+		}
+		
+		return null;
+	}
+
+}
